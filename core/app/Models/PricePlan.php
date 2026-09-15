1db1385ed1bf199873b6f1c111fa1a65558df2b1
moehail967-cpu <moehail967@gmail.com>
2026-09-15 23:48:40 +0300

    fix: restore price plan translations

--- core/app/Models/PricePlan.php
+++ core/app/Models/PricePlan.php
@@ -12,7 +12,9 @@ use Stancl\Tenancy\Database\Concerns\ResourceSyncing;
 
 class PricePlan extends Model
 {
-    use HasFactory;
+    use HasFactory, HasTranslations;
+
+    public $translatable = ['title'];
 
     protected $fillable = ['title','features','type','status','price','free_trial','faq',
         'page_permission_feature','blog_permission_feature','product_permission_feature','storage_permission_feature', 'package_badge', 'package_description',
